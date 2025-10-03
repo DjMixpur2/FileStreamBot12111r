a954873329462c756693d459a4a67cbf3f7564a0
DjMixpur2 <djmixpur2@gmail.com>
2025-10-03 10:18:01 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,9 +11,9 @@ class Telegram:
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 24))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "http://65.108.9.125:8081")
+    BASE_URL = env.get("BASE_URL", "http://vps.hostingup.icu:8080")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
-    PORT = int(env.get("PORT", 8081))
+    PORT = int(env.get("PORT", 8080))
 
 # LOGGING CONFIGURATION
 LOGGER_CONFIG_JSON = {
@@ -54,5 +54,6 @@ LOGGER_CONFIG_JSON = {
         }
     }
 }
+
 
 
